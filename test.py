81df3d6d6f93ffe8b1df7b45b3892771ed23bca2
Ayush <ayush999g@gmail.com>
2018-02-13 05:54:56 +0530

    Added K means Clustering
    Added class K means Clustering to th framework.

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import learnit as lt
 import pandas as pd
 from learnit.preprocessing.normalise import Normaliser
 from learnit.linear_reg.linear_regressor import *
+from learnit.clustering.K_means import *
 nl = Normaliser()
 #a = (np.array([[1,1],[2,1],[3,1]]))
 #print(gradientDescent(a[:][0],a[:][1]))
